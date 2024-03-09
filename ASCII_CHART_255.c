e8f99e359d18c924ae8485f035e0dbee3c508d22
das-rishav <rishavdas.in@gmail.com>
2024-03-09 20:49:55 +0530

    first commit

--- ASCII_CHART_255.c
+++ ASCII_CHART_255.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-int main
+int main()
 {
     int i;
     printf("\t\t AASCII TABLE \n");
     printf("\t Integer value \t ASCII VALUE \n");
     for(i=0;i<=255;i++){
-        printf("%d%c",i,i);
+        printf("%d %c\n",i,i);
     }
-    return 0
+    return 0;
 }
