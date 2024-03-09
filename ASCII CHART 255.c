53cfa14ab9df8819db8b68542e6def02ef12bb3e
Rishav Das <rishavdas.in@gmail.com>
2024-03-09 20:03:02 +0530

    First Commit'

---
+++ ASCII CHART 255.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main
+{
+    int i;
+    printf("\t\t AASCII TABLE \n");
+    printf("\t Integer value \t ASCII VALUE \n");
+    for(i=0;i<=255;i++);
+    printf("%d%c",i,i);
+    return 0
+}
